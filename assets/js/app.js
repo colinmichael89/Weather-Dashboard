24118aba7ba999ac4b98c403d431b198a0320cac
colinmichael89 <colinmichael89@gmail.com>
2022-07-31 23:51:05 -0600

    cleaned up, added forecast function, moment, populated  fields

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,54 +1,78 @@
-// 1) Page loads with data based on users location
-// 2) Button click event listener (id)
-// 3) Use id to fetch api root with query selctector -ex: http://google.com/{id}
-// 4)
-
 // Define Variables
 const APIRoot = "https://api.openweathermap.org/data/2.5/weather";
 const APIKey = "8a1b9ee606209a0862fbdcd28f56a206";
+// const iconImage = "https://openweathermap.org/img/w/" + icon + ".png";
 
 // Query Selectors
+const searchedCity = document.querySelector(".form-input");
 const submitButton = document.querySelector(".submit");
-const currentCity = document.querySelector(".current-city");
+const currentCity = document.querySelector("#current-city");
 const currentDate = document.querySelector(".current-date");
-const currentIcon = document.querySelector(".current-weather-icon");
-const currentTemp = document.querySelector("current-temp");
-const currentWind = document.querySelector("current-wind");
-const currentHumidity = document.querySelector("current-humidity");
-const currentUvIndex = document.querySelector("current-uv-index");
+const currentIcon = document.querySelector("#current-weather-icon");
+const currentTemp = document.querySelector("#current-temp");
+const currentWind = document.querySelector("#current-wind");
+const currentHumidity = document.querySelector("#current-humidity");
+const currentUvIndex = document.querySelector("#current-uv-index");
+const forecastInfo = document.querySelector("#forecast-data");
 
 // Functions
-function getApi() {
-  var queryUrl =
+function currentWeatherApi() {
+  let city = searchedCity.value.trim();
+  var cityQueryUrl =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
-    currentCity +
+    city +
     "&appid=" +
-    APIKey;
+    APIKey +
+    "&units=imperial";
 
-  fetch(queryUrl) // default method = GET
+  fetch(cityQueryUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
+      const currentWeather = data;
+      // Create <li> in .previous-searches <ul>
+      // Searched City is button w/ stored (local, array) Api data that updates page when clicked
+
+      // math.round() below..
+      currentCity.textContent = currentWeather.name;
+      // currentDate.textContent = moment().format("MMMM Do YYYY");
+      // currentCity.iconImage = currentWeather.weather[0];
+      currentTemp.textContent = currentWeather.main.temp + " F";
+      currentWind.textContent = currentWeather.wind.speed + " MPH";
+      currentHumidity.textContent = currentWeather.main.humidity + " %";
+      currentUvIndex.textContent = currentWeather.main.temp;
     });
-  //   // Create <li> in .previous-searches <ul>
-  //   // Searched City is button w/ stored (local, array) Api data that updates page when clicked
-  //   // Simultaneously
-  //   // Populate .current-forecast container:
-  //   // .searched location = (City), (mm/dd/yyy), (Current Weather Icon)
-  //   // .temp = current temp in Farenheit
-  //   // .wind = wind mph
-  //   // .humidity = %
-  //   // .uv-index = x.xx - colored box/icon based on api
-  //   // Simultaneously
-  //   // Populate each individual forecast container:
-  //   // Date, icon, temp, wind, humidity
-  // });
-  //   console.log("Clicked!");
-  //   return;
 }
 
-// special functions - event listeners
-submitButton.addEventListener(`click`, getApi);
-// logic // callbacks
+function forecastApi() {
+  let city = searchedCity.value.trim();
+  var forecastQueryUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&appid=" +
+    APIKey +
+    "&units=imperial";
+
+  fetch(forecastQueryUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    // Need to tell it what info to grab as it loops through data.. Date, icon, temp, wind, humidity
+    // Can I just poplulate forecast containers with the info without individual divs?? Do I need another forLoop to populate containers or do I assign different ids to each container based on index [0 - 4]?????????!!!!!!!!!!
+    .then(function (data) {
+      console.log(data);
+      for (let index = 0; index < 5; index++) {
+        const forecast = data[index];
+        forecastInfo.textContent = forecast;
+      }
+    });
+}
+
+// Special Functions - Event Listeners
+submitButton.addEventListener(`click`, () => {
+  currentWeatherApi();
+  forecastApi();
+});
+// Logic // Callback
